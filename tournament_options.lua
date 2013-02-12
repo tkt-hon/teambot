00757f49324ff38d1706c53233786384c71624f7
Sami Saada <saada@cs.helsinki.fi>
2013-02-12 12:27:45 +0200

    rename tournament option to mode

--- tournament_options.lua
+++ tournament_options.lua
@@ -1,11 +1,11 @@
 local _G = getfenv(0)
-local tournament = _G.object
+local teambot = _G.object
 
-function tournament.Is1v1()
-  local mode = Cvar.GetCvar("tktl_tournament")
+function teambot.Is1v1()
+  local mode = Cvar.GetCvar("teambotmanager_mode")
   return mode and mode:GetString() == "1v1" or false
 end
 
-function tournament.Is5v5()
+function teambot.Is5v5()
   return not tournament.Is1v1()
 end
